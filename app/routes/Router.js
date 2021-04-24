bc89db924fa08324e20f91ed7fb7b8d16b43a5fa
MegalLink <alexresidentevil@gmail.com>
2021-04-23 19:15:05 -0500

    Agregando React Native Elements y Iconos al nav

--- app/routes/Router.js
+++ app/routes/Router.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import RestaurantsStack from './RestaurantsStack'
-import FavoritesStack from './FavoritesStack'
-import TopRestaurantsStack from './TopRestaurantsStack'
-import SearchStack from './SearchStack'
-import AccountStack from './AccountStack'
-
+import RestaurantsStack from './RestaurantsStack';
+import FavoritesStack from './FavoritesStack';
+import TopRestaurantsStack from './TopRestaurantsStack';
+import SearchStack from './SearchStack';
+import AccountStack from './AccountStack';
+import {Icon} from 'react-native-elements';
+const screenOptions=(ruta,color)=>{
+  let iconName;
+  switch(ruta.name){
+    case "restaurants":
+      iconName="compass"
+      break;
+    case "favorites":
+      iconName="heart"
+      break;
+    case "search":
+      iconName="search"
+      break;
+    case "account":
+        iconName="user"
+        break;
+    case "top-restaurants":
+        iconName="star"
+        break;
+    default:
+      break;
+  }
+  return(<Icon  type='font-awesome-5' name={iconName} size={22} color={color}  ></Icon>)
+}
 const Tab = createBottomTabNavigator();
 const Router = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        initialRouteName="restaurants"
+        tabBarOptions={{
+          inactiveTintColor: '#646464',
+          activeTintColor: '#00a686',
+        }}
+        screenOptions={({route})=>({
+          tabBarIcon:({color})=>screenOptions(route,color)
+        })}>
         <Tab.Screen
           name="restaurants"
           component={RestaurantsStack}
@@ -22,18 +53,18 @@ const Router = () => {
           component={FavoritesStack}
           options={{title: 'Favoritos'}}
         />
-       
+
         <Tab.Screen
           name="top-restaurants"
           component={TopRestaurantsStack}
-          options={{title: 'Top'}}
+          options={{title: 'Top 5'}}
         />
         <Tab.Screen
           name="search"
           component={SearchStack}
           options={{title: 'Buscar'}}
         />
-       
+
         <Tab.Screen
           name="account"
           component={AccountStack}
